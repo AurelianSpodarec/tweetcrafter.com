ca1ef88b328a5f791a48fcd739c948f76f43d4aa
Aurelian Spodarec <aurelian.spod@gmail.com>
2022-12-19 22:12:39 +0000

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */ 
+module.exports = {
+  mode: "jit",
+  darkMode: "class",
+  content: [
+      "./src/components/**/*.{js,ts,jsx,tsx}", 
+      "./src/pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+      extend: {},
+  },
+  // plugins: [require("@tailwindcss/typography"),],
+};
