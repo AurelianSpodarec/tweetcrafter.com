08a029fa4b30fc332f0e8c0af4db3a44775ec2d7
Aurelian Spodarec <aurelian.spod@gmail.com>
2022-12-20 21:13:55 +0000

    fix: hydratoin error

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -27,12 +27,9 @@ export default function Editor() {
 
 
                     <div className="p-4">
-                        <button className="w-full" id="headlessui-disclosure-button-:r4:" type="button" aria-expanded="false">
+                        <button className="w-full" type="button" aria-expanded="false">
                             <div className="flex justify-between items-center text-gray-50 mb-3">
                                 <h2 className="text-xs font-medium">Text</h2>
-                                <button className="text-gray-50 h-4 w-4">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-full w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2"><path strokeLinecap="round" strokeLinejoin="round" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                </button>
                             </div>
                         </button>
 
