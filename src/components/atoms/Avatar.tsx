37b4f0c882db444959a4af3613ba92ad9a4e50a6
Aurelian Spodarec <aurelian.spod@gmail.com>
2022-12-21 14:27:24 +0000

    refactor: improved avatar code

--- src/components/atoms/Avatar.tsx
+++ src/components/atoms/Avatar.tsx
@@ -1,41 +1,35 @@
 function Avatar({ 
-        size = 'md', 
-        shape = 'rounded', 
-        src, 
-        alt 
-    }: AvatarProps) {
+    size = 'md', 
+    shape = 'rounded', 
+    src, 
+    alt 
+}: AvatarProps) {
+  const avatarSizes = {
+    sm: 20,
+    md: 48,
+    lg: 133.5,
+  };
 
-    let avatarSize;
-    switch(size) {
-        case 'sm':
-            avatarSize = 20;
-            break;
-        case 'md':
-            avatarSize = 48;
-            break;
-        case 'lg':
-            avatarSize = 133.5;
-            break;
-        default:
-            avatarSize = 48;
-    }
-    if(src === "") return <></>
-    if(src !== undefined)
-    return (
-        <img 
-            src={src}
-            className={`${shape === 'rounded' ? 'rounded-full' : ''} h-12 w-12`}
-            style={{ height: `${avatarSize}px`, width: `${avatarSize}px` }}
-            alt={alt}
-        />
-    )
+  const avatarSize = avatarSizes[size] || avatarSizes.md;
+
+  if (!src) return <></>;
+
+  return (
+    <img 
+      src={src}
+      className={`${shape === 'rounded' ? 'rounded-full' : ''} h-12 w-12`}
+      style={{ height: `${avatarSize}px`, width: `${avatarSize}px` }}
+      alt={alt}
+    />
+  );
 }
 
 export default Avatar;
 
 interface AvatarProps {
-    size?: 'sm' | 'md' | 'lg';
-    shape?: "rounded" | 'rect';
-    src?: string;
-    alt?: string;
+  size?: 'sm' | 'md' | 'lg';
+  shape?: 'rounded' | 'rect';
+  src?: string;
+  alt?: string;
 }
+
