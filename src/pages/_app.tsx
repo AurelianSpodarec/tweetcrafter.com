10a7a3b3fdcceaa905d3e6080fe7f7ad89aa872c
Aurelian Spodarec <aurelian.spod@gmail.com>
2022-12-23 20:12:33 +0000

    add: vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import '../../styles/styles.scss'
 import type { AppProps } from 'next/app'
 import { TweetTabsProvider } from '../context/TweetTabsInfo'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-  
-    <TweetTabsProvider>
-  <Component {...pageProps} />
-  </TweetTabsProvider>
-  )
+    return (
+        <TweetTabsProvider>
+            <Component {...pageProps} />
+            <Analytics />
+        </TweetTabsProvider>
+    )
 }
