9a34d26a3001b7191ce8d5c807195ee0b0309f0e
Aurelian Spodarec <aurelian.spod@gmail.com>
2022-12-26 21:00:14 +0000

    fix: spacing on header on reply and added time

--- src/components/molecules/AvatarName.tsx
+++ src/components/molecules/AvatarName.tsx
@@ -5,8 +5,9 @@ import VerifiedBadge from "../atoms/VerifiedBadge";
 function AvatarName({src, name, handler, verified, className, onClick, shape, type, option}:AvatarNameProps) {
     console.log(type)
     return (
-        <header onClick={onClick} className={`flex space-x-3 mb-4 ${className}`}>
+        <header onClick={onClick} className={`flex space-x-3 ${option === "reply" ? "" : "mb-4"} ${className}`}>
             <Avatar src={src} shape={type} type={type}/>
+            
 
             <div className="flex flex-col">
                 <div className={`flex  ${option === "reply" ? "flex-row items-center space-x-1" : "flex-col"}`}>
@@ -15,6 +16,13 @@ function AvatarName({src, name, handler, verified, className, onClick, shape, ty
                         <VerifiedBadge verified={verified} />
                     </div>
                     <NameHandler handler={handler} />
+                </div>
+            </div>
+
+            <div className="flex flex-row items-baseline">
+                <div className="text-skin-muted inline" style={{ "fontSize" : "15px", "lineHeight": "20px" }}>.</div>
+                <div>
+                    <span className="text-skin-muted">Dec 10</span>
                 </div>
             </div>
         </header>
