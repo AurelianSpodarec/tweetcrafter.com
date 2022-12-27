cef92cb407e85dfb1bf3b54dd374fdf95b515532
Aurelian Spodarec <aurelian.spod@gmail.com>
2022-12-27 12:40:22 +0000

    add: google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,24 @@ import '../../styles/styles.scss'
 import type { AppProps } from 'next/app'
 import { TwitterEditorProvider } from '../context/TwitterEditorInfo'
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script';
+
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <TwitterEditorProvider>
+            <Script async strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+               {/* <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} /> */}
+
+                <Script strategy="lazyOnload">
+                    {`
+                         window.dataLayer = window.dataLayer || [];
+                         function gtag(){dataLayer.push(arguments);}
+                         gtag('js', new Date());
+                       
+                         gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+                    `}
+                </Script>
             <Component {...pageProps} />
             <Analytics />
         </TwitterEditorProvider>
