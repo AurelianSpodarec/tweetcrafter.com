44fbba014ffe86b8f5d7dd030b585297fc44765b
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2024-12-18 17:53:43 +0100

    fix: Added redux provide into global providers as it wasn't used

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,17 +1,20 @@
 'use client'
 
+import { ReduxProvider } from './redux-provider'
 import { ThemeProvider } from './theme-provider'
 
 function AppProvider({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
+    <ReduxProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </ReduxProvider>
   )
 }
 
