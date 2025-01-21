4121d34ccd82465353d40ba74e1a57184684d030
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2025-01-21 15:05:51 +0100

    update: Added dialog and made a chnange to x anatomy fetature

--- src/components/features/XAnatomy/index.tsx
+++ src/components/features/XAnatomy/index.tsx
@@ -2,29 +2,84 @@
 
 import { getNavigationStateOf } from "@/store/features/navigation/navigationSelectors";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog"
+import { useDispatch } from "react-redux";
+import { menuClose } from "@/store/features/navigation/navigationSlice";
 
+
+const nav = [
+  {
+    id: "",
+    name: "Profile",
+    children: [
+      {
+        name: "Default"
+        // Company, User, etc
+      },
+      {
+        name: "Suspended",
+      },
+      {
+        name: "Blocked"
+      },
+      {
+        name: "Hiring"
+      }
+    ]
+    // Suspended, Blocked
+  },
+  {
+    id: "",
+    name: "Tweet",
+    children: [
+      {
+        name: "Reply",
+      },
+      {
+        name: "Stats"
+      },
+      {
+        name: "Reposted" // with thouhts and just reposted
+      },
+      {
+        name: "With Images"
+      },
+      {
+        name: "Context"
+      }
+    ]
+  }
+]
 
 function FeatureXAnatomy() {
   const isOpen = getNavigationStateOf("xanatomy")
+  const dispatch = useDispatch()
 
   return (
-    <div className={`${isOpen ? "" : "hidden"} absolute z-30 top-0 bottom-0 left-1/2 -translate-x-1/2 w-[790px] h-full py-20`}>
-      <div className="bg-[#0c0e11] relative h-full flex">
-        <div className="overflow-auto px-4 space-y-4 w-full">
-
+    <Dialog open={isOpen} onOpenChange={() => dispatch(menuClose("xanatomy"))}>
+      <DialogContent className="w-[790px] bg-[#0c0e11]">
+        
+        <div className="flex items-center justify-between overflow-auto w-full">
           <aside>
-            
-            X Anatomy
-
-            [close]
+            Navigation
           </aside>
           <section>
-
+            MDX maybe?
           </section>
-
+          <aside>
+            Nav
+          </aside>
         </div>
-      </div>
-    </div>
+
+      </DialogContent>
+    </Dialog>
   );
 }
 
